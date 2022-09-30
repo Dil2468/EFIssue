0242c451ad4e4da1f8d31e35890c74f6317e1390
dileep <dileep2468.a@gmail.com>
2022-09-30 13:21:10 +1300

    first

---
+++ EntityProject/DataCollection/EntityDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EntityProject.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityProject.DataCollection
+{
+    public class EntityDbContext:DbContext
+    {
+
+        public EntityDbContext(DbContextOptions<EntityDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Model.Customer> Customers { get; set; }
+    }
+}
---
+++ EntityProject/Model/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace EntityProject.Model
+{
+    public class Customer
+    {
+        public  int CustID { get; set; }
+        public string Name { get; set; }
+    }
+}
